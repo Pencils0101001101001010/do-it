a029f6d47d4705be0678803be44b0663b36816d7
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-08-27 07:15:47 +0200

    Updated controller set for items

--- server/src/controllers/todoItemsControllers.js
+++ server/src/controllers/todoItemsControllers.js
@@ -27,6 +27,12 @@ exports.getItems = async (req, res, next) => {
 exports.createItems = async (req, res, next) => {
   const { id } = req.params; //List id
 
+  const userId = req.userId;
+
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized." });
+  }
+
   const access = await getAccessLevel(id, req.userId);
   //   console.log(`List id ${id} and user requesting it ${req.userId}`);
   if (!access) return res.status(404).json({ error: "List not found." });
@@ -47,6 +53,37 @@ exports.createItems = async (req, res, next) => {
   }
 };
 
-exports.updateItem = async (req, res, next) => {};
+exports.updateItem = async (req, res, next) => {
+  const { id, itemId } = req.params; //id = list_id, itemId = the specific item
+  const { description, is_done, title } = req.body;
+  const userId = req.userId;
+  console.log(`list id: ${id} \n item id: ${itemId}`);
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized." });
+  }
+
+  const access = await getAccessLevel(id, userId);
+
+  if (!access) {
+    return res.status(404).json({ error: "Couldn't find list" });
+  }
+  if (access === "viewer")
+    return res.status(403).json({ error: "Read-only access" });
+
+  try {
+    const result = await pool.query(
+      "UPDATE todo_items SET description = COALESCE($1, description), is_done = COALESCE($2, is_done), title = COALESCE($3, title) WHERE id = $4 AND list_id = $5 RETURNING *",
+      [description, is_done, title, itemId, id],
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Item not found." });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.deleteItem = async (req, res, next) => {};
--- server/src/routes/todoItemsRoutes.js
+++ server/src/routes/todoItemsRoutes.js
@@ -4,7 +4,7 @@ const items = require("../controllers/todoItemsControllers.js");
 
 router.use(requireAuth);
 
-router.patch("/edit-item/:id", items.updateItem);
+router.patch("/:id/item/:itemId", items.updateItem);
 router.delete("/remove-item/:id", items.deleteItem);
 
 module.exports = router;
