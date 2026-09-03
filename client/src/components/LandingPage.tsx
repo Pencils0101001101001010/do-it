ef8110438987f48607b0a296ab4b1df2a77b632b
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-09-03 19:56:47 +0200

    working on landing page layout

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,11 +1,11 @@
+import ContentBody from "./ContentBody";
 import Navbar from "./Navbar";
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
-      <h1>This is the landing page.....</h1>
-      <p>coming soon</p>
+      <ContentBody />
     </div>
   );
 }
