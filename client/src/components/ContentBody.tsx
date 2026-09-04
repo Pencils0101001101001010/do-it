f60131ca7097922b5e9cde3c6cd11544c9f26789
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-09-04 11:48:11 +0200

    consumed create + get list api on frontend

--- client/src/components/ContentBody.tsx
+++ client/src/components/ContentBody.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import Content from "./Content";
 import ContentSidebar from "./ContentSidebar";
+import type { List } from "../types";
+import api from "../../api/client";
+import toast from "react-hot-toast";
+import type { AxiosError } from "axios";
 
 export default function ContentBody() {
+  const [getLists, setGetLists] = useState<List[]>([]);
+
+  useEffect(() => {
+    api.get<List[]>("/list/todos").then((res) => {
+      setGetLists(res.data);
+      if (res.data.length > 0) return <p>No list</p>;
+    });
+  }, []);
+
+  const handleCreateList = async (name: string) => {
+    if (!name) return;
+    await toast.promise(api.post("/list/new", { name: name }), {
+      loading: "Creating...",
+      success: "Done!",
+      error: (err: unknown) => {
+        const axiosErr = err as AxiosError<{ error: string }>;
+        return axiosErr.response?.data?.error || "Failed to create";
+      },
+    });
+  };
   return (
     <div className="drawer lg:drawer-open">
       <input
@@ -21,7 +46,7 @@ export default function ContentBody() {
         ></label>
         <div className="flex min-h-full flex-col items-start   is-drawer-close:w-14 is-drawer-open:w-64">
           {/* Sidebar content here */}
-          <ContentSidebar />
+          <ContentSidebar lists={getLists} onCreate={handleCreateList} />
         </div>
       </div>
     </div>
--- client/src/components/ContentSidebar.tsx
+++ client/src/components/ContentSidebar.tsx
@@ -1,122 +1,63 @@
-export default function ContentSidebar() {
+import { useState, type SubmitEvent } from "react";
+
+import type { List } from "../types";
+
+interface props {
+  lists: List[];
+  onCreate: (name: string) => void;
+}
+
+export default function ContentSidebar({ lists, onCreate }: props) {
+  const [listName, setListName] = useState("");
+
+  const handleCreate = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!listName.trim()) return;
+    onCreate(listName.trim());
+
+    setListName("");
+  };
+
   return (
     <>
-      <ul className="menu w-full grow">
-        <li></li>
+      <ul className="menu w-full grow overflow-hidden">
         <li>
-          <button
-            className="is-drawer-close:tooltip is-drawer-close:tooltip-right "
-            data-tip="Homepage"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-              className="my-1.5 inline-block size-4"
-            >
-              <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-              <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            </svg>
-            <span className="is-drawer-close:hidden">Homepage</span>
-          </button>
+          <form onSubmit={handleCreate}>
+            {" "}
+            <button type="submit">Add</button>
+            <input
+              type="text"
+              name="name"
+              value={listName}
+              onChange={(e) => setListName(e.target.value)}
+              className="w-45 border border-gray-100"
+            />{" "}
+          </form>
         </li>
-
-        <li>
-          <button
-            className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-            data-tip="Settings"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-              className="my-1.5 inline-block size-4"
+        {lists.map((l) => (
+          <li key={l.id}>
+            <button
+              className="is-drawer-close:tooltip is-drawer-close:tooltip-right "
+              data-tip="Homepage"
             >
-              <path d="M20 7h-9"></path>
-              <path d="M14 17H5"></path>
-              <circle cx="17" cy="17" r="3"></circle>
-              <circle cx="7" cy="7" r="3"></circle>
-            </svg>
-            <span className="is-drawer-close:hidden">Settings</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-            data-tip="Settings"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-              className="my-1.5 inline-block size-4"
-            >
-              <path d="M20 7h-9"></path>
-              <path d="M14 17H5"></path>
-              <circle cx="17" cy="17" r="3"></circle>
-              <circle cx="7" cy="7" r="3"></circle>
-            </svg>
-            <span className="is-drawer-close:hidden">Settings</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-            data-tip="Settings"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-              className="my-1.5 inline-block size-4"
-            >
-              <path d="M20 7h-9"></path>
-              <path d="M14 17H5"></path>
-              <circle cx="17" cy="17" r="3"></circle>
-              <circle cx="7" cy="7" r="3"></circle>
-            </svg>
-            <span className="is-drawer-close:hidden">Settings</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-            data-tip="Settings"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-              className="my-1.5 inline-block size-4"
-            >
-              <path d="M20 7h-9"></path>
-              <path d="M14 17H5"></path>
-              <circle cx="17" cy="17" r="3"></circle>
-              <circle cx="7" cy="7" r="3"></circle>
-            </svg>
-            <span className="is-drawer-close:hidden">Settings</span>
-          </button>
-        </li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2"
+                fill="none"
+                stroke="currentColor"
+                className="my-1.5 inline-block size-4"
+              >
+                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
+                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              </svg>
+              <span className="is-drawer-close:hidden">{l.name}</span>
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
